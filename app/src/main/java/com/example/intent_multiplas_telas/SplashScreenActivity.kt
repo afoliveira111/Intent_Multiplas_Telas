131423230ab8d9ed8a0760d0d8ca456921e32cb1
Felipe <felipetermometria@hotmail.com>
2023-11-29 18:40:44 -0300

    Splash Screen create

---
+++ app/src/main/java/com/example/intent_multiplas_telas/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.intent_multiplas_telas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
