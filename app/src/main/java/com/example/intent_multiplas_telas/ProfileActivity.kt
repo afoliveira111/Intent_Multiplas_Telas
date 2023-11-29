5c91e7808160d5b4bcd0dc4eca8f9772fedeb971
Felipe <felipetermometria@hotmail.com>
2023-11-29 18:20:34 -0300

    Initial commit

---
+++ app/src/main/java/com/example/intent_multiplas_telas/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.intent_multiplas_telas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
