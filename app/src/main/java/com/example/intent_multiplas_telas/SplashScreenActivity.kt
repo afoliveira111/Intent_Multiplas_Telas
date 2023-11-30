9229f5fc4fc3b01e7a5d871cf25fda40bf14dd16
Felipe <felipetermometria@hotmail.com>
2023-11-30 12:26:07 -0300

    Splash Screen update

--- app/src/main/java/com/example/intent_multiplas_telas/SplashScreenActivity.kt
+++ app/src/main/java/com/example/intent_multiplas_telas/SplashScreenActivity.kt
@@ -1,11 +1,20 @@
 package com.example.intent_multiplas_telas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+           val i = Intent(this, MainActivity::class.java)
+           startActivity(i)
+            finish()
+        },1000)
     }
 }
